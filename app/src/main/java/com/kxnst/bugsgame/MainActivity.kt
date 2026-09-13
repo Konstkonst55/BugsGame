721b6392899ba7dafdc2d34369b254f81e59e81c
Константин <konstkonst55@mail.ru>
2026-09-13 23:27:40 +0700

    feat: update main activity with toolbar

--- app/src/main/java/com/kxnst/bugsgame/MainActivity.kt
+++ app/src/main/java/com/kxnst/bugsgame/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.kxnst.bugsgame
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.kxnst.bugsgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.root.post {
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(binding.fcvNavHost.id) as NavHostFragment
+            val navController = navHostFragment.navController
+
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                binding.tbMain.title = destination.label
+
+                binding.tbMain.navigationIcon = null
+                binding.tbMain.setNavigationOnClickListener(null)
+            }
         }
     }
 }
