88d447bc5caa9a9b3e6a2f54356431966d48cb3e
Константин <konstkonst55@mail.ru>
2026-09-15 00:54:18 +0700

    feat: add bottom navigation

--- app/src/main/java/com/kxnst/bugsgame/MainActivity.kt
+++ app/src/main/java/com/kxnst/bugsgame/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.kxnst.bugsgame
 
 import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+
 import com.kxnst.bugsgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +24,43 @@ class MainActivity : AppCompatActivity() {
                 supportFragmentManager.findFragmentById(binding.fcvNavHost.id) as NavHostFragment
             val navController = navHostFragment.navController
 
-            navController.addOnDestinationChangedListener { _, destination, _ ->
-                binding.tbMain.title = destination.label
+            binding.bnvMain.setupWithNavController(navController)
+            binding.tbMain.inflateMenu(R.menu.menu_toolbar)
 
-                binding.tbMain.navigationIcon = null
-                binding.tbMain.setNavigationOnClickListener(null)
+            binding.tbMain.setOnMenuItemClickListener { item ->
+                if (item.itemId == R.id.action_toolbar_settings) {
+                    navController.navigate(R.id.action_global_settings)
+                    true
+                } else {
+                    false
+                }
+            }
+
+            navController.addOnDestinationChangedListener { controller, destination, _ ->
+                val isSettings = destination.id == R.id.settingsFragment
+                val isBottomDestination = destination.id in setOf(
+                    R.id.homeFragment,
+                    R.id.rulesFragment,
+                    R.id.authorsFragment
+                )
+
+                binding.tbMain.title = destination.label
+                binding.bnvMain.isVisible = isBottomDestination
+                binding.tbMain.menu.findItem(R.id.action_toolbar_settings).isVisible = isBottomDestination
+
+                if (isSettings) {
+                    binding.tbMain.navigationIcon = AppCompatResources.getDrawable(
+                        this,
+                        androidx.appcompat.R.drawable.abc_ic_ab_back_material
+                    )
+                    binding.tbMain.setNavigationOnClickListener {
+                        controller.navigateUp()
+                    }
+                } else {
+                    binding.tbMain.navigationIcon = null
+                    binding.tbMain.setNavigationOnClickListener(null)
+                }
+
             }
         }
     }
--- app/src/main/java/com/kxnst/bugsgame/di/AppModule.kt
+++ app/src/main/java/com/kxnst/bugsgame/di/AppModule.kt
@@ -1,15 +1,22 @@
 package com.kxnst.bugsgame.di
 
+import com.kxnst.bugsgame.data.settings.GameSettingsRepository
+import com.kxnst.bugsgame.data.settings.SharedPreferencesGameSettingsRepository
 import com.kxnst.bugsgame.data.zodiac.AndroidResourceZodiacRepository
 import com.kxnst.bugsgame.data.zodiac.ZodiacRepository
 import com.kxnst.bugsgame.domain.zodiac.ZodiacCalculator
 import com.kxnst.bugsgame.presentation.register.PlayerViewModel
+import com.kxnst.bugsgame.presentation.settings.GameSettingsViewModel
+
 import org.koin.android.ext.koin.androidContext
-import org.koin.dsl.module
 import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
 
 val appModule = module {
     single<ZodiacRepository> { AndroidResourceZodiacRepository(androidContext().resources) }
     single { ZodiacCalculator() }
+    single<GameSettingsRepository> { SharedPreferencesGameSettingsRepository(androidContext()) }
+
     viewModel { PlayerViewModel(get(), get()) }
+    viewModel { GameSettingsViewModel(get()) }
 }
