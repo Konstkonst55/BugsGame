56cc35b438af0fffb20ae6324a2c5355699b91ec
Константин <konstkonst55@mail.ru>
2026-09-21 22:51:08 +0700

    refactor: update zodiac icons resources

--- app/src/main/java/com/kxnst/bugsgame/data/zodiac/AndroidResourceZodiacRepository.kt
+++ app/src/main/java/com/kxnst/bugsgame/data/zodiac/AndroidResourceZodiacRepository.kt
@@ -1,10 +1,12 @@
 package com.kxnst.bugsgame.data.zodiac
 
 import android.content.res.Resources
-import android.util.Xml
+
 import com.kxnst.bugsgame.R
+
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+
 import org.xmlpull.v1.XmlPullParser
 
 class AndroidResourceZodiacRepository(
@@ -18,11 +20,17 @@ class AndroidResourceZodiacRepository(
             while (it.next() != XmlPullParser.END_DOCUMENT) {
                 if (it.eventType == XmlPullParser.START_TAG && it.name == "sign") {
                     result += ZodiacSign(
-                        name = resources.getString(it.getAttributeResourceValue(null, "name", 0)),
+                        name = resources.getString(
+                            it.getAttributeResourceValue(null, "name", 0)
+                        ),
                         contentDescription = resources.getString(
                             it.getAttributeResourceValue(null, "contentDescription", 0)
                         ),
-                        iconResourceName = it.getAttributeValue(null, "icon").orEmpty(),
+                        iconResourceId = it.getAttributeResourceValue(
+                            null,
+                            "icon",
+                            0
+                        ),
                         monthStart = it.getAttributeIntValue(null, "monthStart", 1),
                         dayStart = it.getAttributeIntValue(null, "dayStart", 1),
                         monthEnd = it.getAttributeIntValue(null, "monthEnd", 1),
@@ -31,6 +39,7 @@ class AndroidResourceZodiacRepository(
                 }
             }
         }
+
         result
     }
 }
